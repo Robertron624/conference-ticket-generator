ee0f277fa3f423b4e5085ac67d11035e65b35811
Robertron624 <robert.624@hotmail.com>
2025-01-14 16:40:15 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  console.log("Hello Vite + Svelte!");
+</script>
+
+<main>
+  <h1>Hello Vite + Svelte!</h1>
+</main>
+
+<style lang="scss">
+
+  @use "./variables" as vars;
+
+  main {
+    h1 {
+      color: vars.$neutral-300;
+      background-color: vars.$neutral-900;
+      padding: 1rem;
+      border-radius: 0.5rem;
+    }
+  }
+</style>
